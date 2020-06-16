759056fc2f21eb33ebf895b69f50bacf2934751e
Odagawa <xtctl.1234@gmail.com>
2020-06-17 02:05:53 +0900

    resize icon

--- src/components/player/Index.vue
+++ src/components/player/Index.vue
@@ -131,7 +131,7 @@ export default {
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
     0 3px 1px -2px rgba(0, 0, 0, 0.2);
   transition: 0.3s ease-out;
-  width: 50px;
+  width: 35px;
   text-align: center; /*一応BOX内の文字も中央寄せ*/
   &:hover {
     cursor: pointer;
